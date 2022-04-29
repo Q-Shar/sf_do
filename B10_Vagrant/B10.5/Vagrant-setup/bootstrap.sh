ccc32ae089afc831ed9b7cd553af8b89b25fd43f
Q-Shar <dencher66@gmail.com>
2022-04-29 18:39:17 +0500

    + B10.5 Vagrant + PostgreSQL

---
+++ B10_Vagrant/B10.5/Vagrant-setup/bootstrap.sh
@@ -0,0 +1,107 @@
+#!/bin/sh -e
+
+# Edit the following to change the name of the database user that will be created:
+export APP_DB_USER=${DB_USER}
+export APP_DB_PASS=${DB_PASSWORD}
+##APP_DB_USER=myapp
+##APP_DB_PASS=dbpass
+
+# Edit the following to change the name of the database that is created (defaults to the user name)
+export APP_DB_NAME=${DB_NAME}
+
+# Edit the following to change the version of PostgreSQL that is installed
+export PG_VERSION=${PG_VERSION}
+export DB_PORT_HOST=${DB_PORT_HOST}
+
+###########################################################
+# Changes below this line are probably not necessary
+###########################################################
+print_db_usage () {
+  echo "Your PostgreSQL database has been setup and can be accessed on your local machine on the forwarded port"
+  echo "  Host: localhost"
+  echo "  Port: $DB_PORT_HOST"
+  echo "  Database: $APP_DB_NAME"
+  echo "  Username: $APP_DB_USER"
+  echo "  Password: $APP_DB_PASS"
+  echo ""
+  echo "Admin access to postgres user via VM:"
+  echo "  vagrant ssh"
+  echo "  sudo su - postgres"
+  echo ""
+  echo "psql access to app database user via VM:"
+  echo "  vagrant ssh"
+  echo "  sudo su - postgres"
+  echo "  PGUSER=$APP_DB_USER PGPASSWORD=$APP_DB_PASS psql -h localhost $APP_DB_NAME"
+  echo ""
+  echo "Env variable for application development:"
+  echo "  DATABASE_URL=postgresql://$APP_DB_USER:$APP_DB_PASS@localhost:$DB_PORT_HOST/$APP_DB_NAME"
+  echo ""
+  echo "Local command to access the database via psql:"
+  echo "  PGUSER=$APP_DB_USER PGPASSWORD=$APP_DB_PASS psql -h localhost -p $DB_PORT_HOST $APP_DB_NAME"
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+PROVISIONED_ON=/etc/vm_provision_on_timestamp
+if [ -f "$PROVISIONED_ON" ]
+then
+  echo "VM was already provisioned at: $(cat $PROVISIONED_ON)"
+  echo "To run system updates manually login via 'vagrant ssh' and run 'apt-get update && apt-get upgrade'"
+  echo ""
+  print_db_usage
+  exit
+fi
+
+PG_REPO_APT_SOURCE=/etc/apt/sources.list.d/pgdg.list
+if [ ! -f "$PG_REPO_APT_SOURCE" ]
+then
+  # Add PG apt repo:
+  # Important: "bionic-pgdg" - must exist in postgresql repo!
+  echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > "$PG_REPO_APT_SOURCE"
+
+  # Add PGDG repo key:
+  # Important: use --no-check-certificate
+  wget --no-check-certificate --quiet -O - https://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
+fi
+
+# Update package list and upgrade all packages
+apt-get update
+apt-get -y upgrade
+
+apt-get -y install "postgresql-$PG_VERSION" "postgresql-contrib-$PG_VERSION"
+
+PG_CONF="/etc/postgresql/$PG_VERSION/main/postgresql.conf"
+PG_HBA="/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
+PG_DIR="/var/lib/postgresql/$PG_VERSION/main"
+
+# Edit postgresql.conf to change listen address to '*':
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
+
+# Append to pg_hba.conf to add password auth:
+# Important: use actual syntax for the provided postgresql version!
+echo "host    all             all             0.0.0.0/0                    md5" >> "$PG_HBA"
+
+# Explicitly set default client_encoding
+echo "client_encoding = utf8" >> "$PG_CONF"
+
+# Restart so that all new config is loaded:
+service postgresql restart
+
+cat << EOF | su - postgres -c psql
+-- Create the database user:
+CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
+
+-- Create the database:
+CREATE DATABASE $APP_DB_NAME WITH OWNER=$APP_DB_USER
+                                  LC_COLLATE='en_US.utf8'
+                                  LC_CTYPE='en_US.utf8'
+                                  ENCODING='UTF8'
+                                  TEMPLATE=template0;
+EOF
+
+# Tag the provision time:
+date > "$PROVISIONED_ON"
+
+echo "Successfully created PostgreSQL dev virtual machine."
+echo ""
+print_db_usage
